85245511757308747f244a658a8148ba964599d4
Squidward-cyber <117612267+Squidward-cyber@users.noreply.github.com>
2024-08-24 06:22:51 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 print("to replace http-server")
 
+from flask import render_template, Flask
 
+app=Flask(__name__)
 
+@app.route("/")
 
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
+    
